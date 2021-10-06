b33eea5903b73ddf92411a05db20742b26c7e06a
Vybhavashri <46229037+Vybhavashri@users.noreply.github.com>
2021-10-06 04:25:07 +0200

    Create script.js
    If applied, this commit will have JQuery script for ToDo list app

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function newItem() {
+
+  //1. Adding a new item to the list:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    let list = $('#list');
+    list.append(li);
+  }
+
+  //2. Crossing out an item from the list of items:
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  //3(i). Adding the delete button "X":
+  let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", function deleteListItem() {
+    li.addClass("delete");
+  });
+
+  // 4. Reordering the items:
+  $('#list').sortable();
+}
